efc5d48fbf596b0f92a1ff0a3dfb1ef51a8740ef
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-02-01 12:32:39 +0900

    Rename: Rename project name to TokTakTokTak

--- TokTakTokTak.swiftpm/Package.swift
+++ TokTakTokTak.swiftpm/Package.swift
@@ -8,19 +8,19 @@ import PackageDescription
 import AppleProductTypes
 
 let package = Package(
-    name: "Hangeul_Watch",
+    name: "TokTakTokTak",
     platforms: [
         .iOS("16.0")
     ],
     products: [
         .iOSApplication(
-            name: "Hangeul_Watch",
+            name: "TokTakTokTak",
             targets: ["AppModule"],
-            bundleIdentifier: "com.yuseong.com.Hangeul-Watch",
+            bundleIdentifier: "com.yuseong.com.TokTakTokTak",
             teamIdentifier: "Q6CK9777WL",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .moon),
+            appIcon: .placeholder(icon: .flower),
             accentColor: .presetColor(.red),
             supportedDeviceFamilies: [
                 .pad,
