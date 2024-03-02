f91df6a3214f9440bd3b4199ec4a52598c534c8f
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-03-02 16:07:35 +0900

    Feat: #9 Create Projects

---
+++ myot-si-ya/myot-si-ya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myot-si-ya
+//
+//  Created by 남유성 on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myot-si-ya/myot-si-ya/myot_si_yaApp.swift
@@ -0,0 +1,17 @@
+//
+//  myot_si_yaApp.swift
+//  myot-si-ya
+//
+//  Created by 남유성 on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct myot_si_yaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
